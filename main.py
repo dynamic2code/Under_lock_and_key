9ed1d34acd7b7c4cc2e5e060424f8a3c5ba5d79f
dynamic2code <dynamicdesingns@gmail.com>
2023-06-17 20:38:36 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def sender():
+    return render_template('sender.html')
+
+
+if __name__ == '__main__':
+   app.run()
