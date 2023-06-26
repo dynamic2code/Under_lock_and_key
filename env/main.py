2d06f09c02c2998a86e4753b4f4ebe16c1e6689f
dynamic2code <dynamicdesingns@gmail.com>
2023-06-26 20:49:22 +0300

    form and receiving

--- env/main.py
+++ env/main.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def sender():
     return render_template('sender.html')
+
+@app.route('/sent')
+def sent():
+    """
+    receives the upload form and directs the user to a status page 
+    """
+    name = request.form['name']
+    receiver = request.form['receiver']
+    cost = request.form['cost']
+    account = request.form['payment_method']
+
+    
+
 
 
 if __name__ == '__main__':
