7ed58b80c0c09d87e5f00e74a50649d9d0301623
dynamic2code <dynamicdesingns@gmail.com>
2023-06-26 21:05:31 +0300

    registration form

--- env/main.py
+++ env/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, redirect, request
+# from flask_sslify import SSLify
 
 app = Flask(__name__)
+# sslify = SSLify(app)
 
 @app.route('/')
 def sender():
@@ -16,9 +18,10 @@ def sent():
     cost = request.form['cost']
     account = request.form['payment_method']
 
-    
+
 
 
 
 if __name__ == '__main__':
    app.run()
+#    app.run(ssl_context=('path/to/certificate.crt', 'path/to/private.key'))
