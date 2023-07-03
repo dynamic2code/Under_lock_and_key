eb9dab0d67185cde56e4aa9541879153009df7ad
dynamic2code <dynamicdesingns@gmail.com>
2023-07-03 20:02:19 +0300

    registration and addition of db

--- env/main.py
+++ env/main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, flash, session
 from verifier import verifier
 from compressor import file_divider, file_encrypting
 import os
+import sqlite3
 # from flask_sslify import SSLify
 
 app = Flask(__name__)
@@ -12,13 +13,34 @@ venv_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 @app.route('/')
 def sender():
-    return render_template('sender.html')
+    # if user exist
+    files = "to do"
+    return render_template('sender.html', files = files)
 
 @app.route('/register')
 def register():
     """
     user registration
     """
+    alias = request.form['alias']
+    password = request.form['password']
+
+    # Connect to the SQLite database
+    conn = sqlite3.connect('app.db')
+    cursor = conn.cursor()
+
+    # Check if the name exists in the database
+    cursor.execute("SELECT * FROM user WHERE alias=?", (alias,))
+    existing_user = cursor.fetchone()
+    
+    if existing_user is None:
+        # Save the name to the database
+        cursor.execute("INSERT INTO user (alias) VALUES (?)", (alias,))
+        conn.commit()
+        session['alias'] = alias 
+        flash("Name saved to the database.") 
+    else:
+        flash("Name already exists in the database.") 
 
 @app.route('/sent', methods=['POST'])
 def sent():
