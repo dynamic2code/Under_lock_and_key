ef0f8d1c13ae410002e1aaa43112b32c041595ec
dynamic2code <dynamicdesingns@gmail.com>
2023-07-03 13:31:14 +0300

    changes to file

--- env/main.py
+++ env/main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, redirect, request
+from verifier import verifier
+from compressor import file_divider, file_encrypting
 # from flask_sslify import SSLify
 
 app = Flask(__name__)
@@ -28,7 +30,17 @@ def sent():
 
     print(type(file))
 
-    return render_template('status.html')
+    verified = verifier(file)
+    viewed = 'No'
+    payed = 'No'
+
+    if verified == True:
+        divided_files = [file_divider(file)]
+        file_encrypting(divided_files[1])
+
+
+    status = [verified, viewed, payed]
+    return render_template('status.html', status = status)
 
 
 
---
+++ env/verifier.py
@@ -0,0 +1,7 @@
+# using an AI to be developed we verify the work submitted is same as on the description
+
+def verifier (file):
+
+    verified = True
+
+    return verified
